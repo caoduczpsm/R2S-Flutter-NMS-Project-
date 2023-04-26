71a911b0cda3eb613b61452928d52da05f68be9a
caoduczpsm <nguyencaoduc0922@gmail.com>
2023-04-27 02:23:47 +0700

    done hash password

--- lib/controller/UserController.dart
+++ lib/controller/UserController.dart
@@ -1,5 +1,8 @@
-import 'package:note_management_system/db/UserDatabase.dart';
+import 'dart:convert';
 
+import 'package:note_management_system/db/UserDatabase.dart';
+// ignore: depend_on_referenced_packages
+import 'package:crypto/crypto.dart';
 import '../model/User.dart';
 
 
@@ -19,7 +22,7 @@ class UserController {
     if (await UserSqlHelper.checkEmailAlreadyUsed(email)){
       User user = User(
         email: email,
-        password: password,
+        password: hassPassword(password),
       );
       UserSqlHelper.createUser(user);
       return true;
@@ -28,11 +31,17 @@ class UserController {
   }
 
   Future<int?> login(String email, String password) async{
-    final user = await UserSqlHelper.getUserByEmailPassword(email, password);
+    final user = await UserSqlHelper.getUserByEmailPassword(email, hassPassword(password));
     if (user != null){
       return user.id;
     } else {
       return null;
     }
+  }
+
+  String hassPassword(String password){
+    var bytes = utf8.encode(password);
+    var digest = sha256.convert(bytes);
+    return digest.toString();
   }
 }
