3374138cd380ec15bae2aae8144c433ccc05d803
LAPTOP-2R8EUK8A\mango <mangocdinh321@gmail.com>
2023-05-01 15:27:19 +0700

    add navigation

---
+++ lib/ultilities/page_routes.dart
@@ -0,0 +1,13 @@
+import 'package:note_management_system/dashboard_page/item.dart';
+import 'package:note_management_system/form/Category.dart';
+import 'package:note_management_system/form/NoteScreen.dart';
+import 'package:note_management_system/form/Priority.dart';
+import 'package:note_management_system/form/Status_Form.dart';
+
+class pageRoutes{
+  // static const String dashboard=ItemDashboard.routeName;
+  static const String categoty=CategoryScreen.routeName;
+  static const String note=NoteScreen.routeName;
+  static const String priority=PriorityScreen.routeName;
+  static const String status=StatusScreen.routeName;
+}
